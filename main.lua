db9d2e6c00ed7bd69b20e8c2a1863f275211fde6
cryst1e <97918490+cryst1e@users.noreply.github.com>
2023-02-09 16:23:24 +1300

    Update main.lua

--- main.lua
+++ main.lua
@@ -672,15 +672,94 @@ function serverHop()
 		end
 	end
 
-	while task.wait() do
-		pcall(function()
-			TPReturner(gameId)
-			if foundAnything ~= "" then
-				TPReturner(gameId)
-			end
-		end)
-	end
+	TPReturner(gameId)
+
+	game:GetService("TeleportService").TeleportInitFailed:Connect(function()
+		TPReturner(gameId)
+	end)
 end
+
+task.spawn(function()
+	while task.wait(0.25) do
+		if game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt") and game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt"):FindFirstChild("MessageArea") and game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt"):FindFirstChild("MessageArea"):FindFirstChild("ErrorFrame") and game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt"):FindFirstChild("MessageArea"):FindFirstChild("ErrorFrame"):FindFirstChild("ErrorMessage") then
+			print(game.CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text)
+			local AllIDs = {}
+			local foundAnything = ""
+			local actualHour = os.date("!*t").hour
+			local Deleted = false
+			local S_T = game:GetService("TeleportService")
+			local S_H = game:GetService("HttpService")
+			local RandomName = tostring(math.random(1, 999999))
+			local File = pcall(function()
+				AllIDs = S_H:JSONDecode(readfile(RandomName .. ".json"))
+			end)
+			if not File then
+				table.insert(AllIDs, actualHour)
+				pcall(function()
+					writefile(RandomName .. ".json", S_H:JSONEncode(AllIDs))
+				end)
+			end
+			local function TPReturner(placeId)
+				local Site;
+				if foundAnything == "" then
+					Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
+				else
+					Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+				end
+				local ID = ""
+				if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+					foundAnything = Site.nextPageCursor
+				end
+				local num = 0;
+				for i, v in pairs(Site.data) do
+					local Possible = true
+					ID = tostring(v.id)
+					if tonumber(v.maxPlayers) > tonumber(v.playing) and tonumber(v.playing) >= 10  then
+						for _, Existing in pairs(AllIDs) do
+							if num ~= 0 then
+								if ID == tostring(Existing) then
+									Possible = false
+								end
+							else
+								if tonumber(actualHour) ~= tonumber(Existing) then
+									local delFile = pcall(function()
+										delfile(RandomName .. ".json")
+										AllIDs = {}
+										table.insert(AllIDs, actualHour)
+									end)
+								end
+							end
+							num = num + 1
+						end
+						if Possible == true then
+							table.insert(AllIDs, ID)
+							wait()
+							pcall(function()
+								writefile(RandomName .. ".json", S_H:JSONEncode(AllIDs))
+								wait()
+								S_T:TeleportToPlaceInstance(placeId, ID, game.Players.LocalPlayer)
+							end)
+							wait(4)
+						end
+					end
+				end
+			end
+			function serverHop()
+				local gameId
+				gameId = game.PlaceId
+				while wait() do
+					pcall(function()
+						TPReturner(gameId)
+						if foundAnything ~= "" then
+							TPReturner(gameId)
+						end
+					end)
+				end
+			end
+			serverHop()
+		end
+	end
+end)
 
 local function waitServerHop()
 	task.wait(getgenv().settings.serverHopDelay * 60)
